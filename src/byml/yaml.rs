48e8a237be5c1dbfbcceab1f5aa448a1ec5fc75f
Caleb Smith <email@calebdixonsmith.top>
2022-07-27 13:01:53 -0500

    Work on BYML to text

--- src/byml/yaml.rs
+++ src/byml/yaml.rs
@@ -1,12 +1,17 @@
 use super::*;
 use crate::{yaml::*, Error, Result};
 use ryml::{NodeRef, Tree};
-use smartstring::alias::String;
 
 impl Byml {
     #[allow(missing_docs)]
     pub fn from_text(text: impl AsRef<str>) -> Result<Byml> {
         Parser::new(text.as_ref())?.parse()
+    }
+
+    /// Serialize the document to YAML. This can only be done for Null, Array,
+    /// or Hash nodes.
+    pub fn to_text(&self) -> Result<std::string::String> {
+        Emitter::new(self).emit()
     }
 }
 
@@ -83,6 +88,69 @@ impl<'a> Parser<'a> {
     }
 }
 
+struct Emitter<'a, 'b>(&'a Byml, Tree<'b>);
+
+impl<'a, 'b> Emitter<'a, 'b> {
+    fn new(byml: &'a Byml) -> Self {
+        let mut tree = Tree::default();
+        tree.reserve(10000);
+        Self(byml, tree)
+    }
+
+    fn build_node<'e>(
+        byml: &Byml,
+        mut dest_node: NodeRef<'b, 'e, '_, &'e mut Tree<'b>>,
+    ) -> Result<()> {
+        match byml {
+            Byml::Array(array) => {
+                dest_node.set_type_flags(ryml::NodeType::Seq)?;
+                for item in array {
+                    let node = dest_node.append_child()?;
+                    Self::build_node(item, node)?;
+                }
+            }
+            Byml::Hash(hash) => {
+                dest_node.set_type_flags(ryml::NodeType::Map)?;
+                for (key, value) in hash {
+                    let mut node = dest_node.append_child()?;
+                    node.set_key(key)?;
+                    Self::build_node(value, node)?;
+                }
+            }
+            Byml::String(s) => dest_node.set_val(s)?,
+            Byml::Bool(b) => dest_node.set_val(if *b { "true" } else { "false" })?,
+            Byml::Float(f) => dest_node.set_val(&lexical::to_string(*f))?,
+            Byml::Double(d) => dest_node.set_val(&lexical::to_string(*d))?,
+            Byml::I32(i) => dest_node.set_val(&lexical::to_string(*i))?,
+            Byml::I64(i) => dest_node.set_val(&lexical::to_string(*i))?,
+            Byml::U32(u) => dest_node.set_val(&lexical::to_string(*u))?,
+            Byml::U64(u) => dest_node.set_val(&lexical::to_string(*u))?,
+            Byml::Null => dest_node.set_val("null")?,
+            Byml::BinaryData(data) => {
+                dest_node.set_val_tag("!!binary")?;
+                dest_node.set_val(&base64::encode(&data))?;
+            }
+        }
+        Ok(())
+    }
+
+    fn emit(self) -> Result<std::string::String> {
+        let Self(byml, mut tree) = self;
+        match byml {
+            Byml::Hash(_) => tree.to_map(0)?,
+            Byml::Array(_) => tree.to_seq(0)?,
+            Byml::Null => return Ok("null".to_string()),
+            _ => {
+                return Err(Error::Any(
+                    "Can only serialize Hash, Array, or Null nodes to YAML".into(),
+                ));
+            }
+        };
+        Self::build_node(byml, tree.root_ref_mut()?)?;
+        Ok(tree.emit()?)
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -110,6 +178,21 @@ mod test {
                     assert_eq!(v1, v2);
                 }
             }
+        }
+    }
+
+    #[test]
+    fn text_roundtrip() {
+        for file in crate::byml::FILES {
+            println!("{}", file);
+            let text = std::fs::read_to_string(
+                std::path::Path::new("test/byml").join([file, ".yml"].join("")),
+            )
+            .unwrap();
+            let byml = Byml::from_text(text).unwrap();
+            let text = byml.to_text().unwrap();
+            let byml = Byml::from_text(text).unwrap();
+            assert_eq!(byml, byml);
         }
     }
 }
